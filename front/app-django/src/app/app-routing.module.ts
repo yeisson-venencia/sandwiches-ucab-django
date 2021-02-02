480141a101c7fdd5b85dc3d14fce06e7c3dc4d88
Roberto Carbajales <robertocd98@gmail.com>
2021-02-02 11:27:59 -0400

    PR2-003 agregando archivo de rutas, mas componentes de home y page not found basicos

---
+++ front/app-django/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+
+const routes: Routes = [
+
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: NopagefoundComponent }
+
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot( routes )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
