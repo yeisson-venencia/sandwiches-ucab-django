cae3dec26aa259291d130cf90fc533eadcd13e3b
Yeisson Venencia <yvenencia.saurith@gmail.com>
2021-02-02 00:38:13 -0400

    PR2-001: agreganda url de presentacion de la api

--- api/sandwiches/views.py
+++ api/sandwiches/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'Example' : '/example/',
+        'Create': '/create/',
+        'Update': '/update/',
+        'Delete': '/delete/',
+    }
+
+    return Response(api_urls)
