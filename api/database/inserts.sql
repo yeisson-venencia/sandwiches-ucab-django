c2062ea93e58280f94a30f13991d8a4beba69c64
Yeisson Venencia <yvenencia.saurith@gmail.com>
2021-02-02 01:30:19 -0400

    PR2-001: creado archivo de insert para base de datos

---
+++ api/database/inserts.sql
@@ -0,0 +1,15 @@
+insert into sandwiches_ingredient (name, price)
+values 
+('Jamon', 40.00),
+('Champignones', 35.00),
+('Pimenton', 30.00),
+('Doble Queso', 40.00),
+('Aceitunas', 57.50),
+('Pepperoni', 38.50),
+('Salchichon', 62.50);
+
+insert into sandwiches_size (name, price)
+values 
+('Individual', 280.00),
+('Doble', 430.00),
+('Triple', 580.00);
