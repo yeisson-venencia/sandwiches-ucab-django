12082c43ab8eab99adf0477c048c4806bc5ec10d
Roberto Carbajales <robertocd98@gmail.com>
2021-02-02 13:43:24 -0400

    PR2-003: home component con estilos terminado y funcionalidad en boton hacer orden

--- front/app-django/src/app/pages/home/home.component.ts
+++ front/app-django/src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  hacerOrden(){
+    this.router.navigate(['/order'])
   }
 
 }
